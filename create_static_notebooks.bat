fd3f185b8bbde18261f1ab4d4cbf39ff211c83ff
Aleksei Olenev <lex.olenev@gmail.com>
2024-12-21 23:20:43 +0100

    Added the static versions of all the notebooks in separate folder static

---
+++ create_static_notebooks.bat
@@ -0,0 +1,24 @@
+@echo off
+
+REM Ensure the subfolder "static" exists
+if not exist "static" mkdir "static"
+
+REM Call the function to create and modify files
+CALL :createAndModifyFile "pulse-modulation_pt1.ipynb" "static\pulse-modulation_pt1-static.ipynb"
+CALL :createAndModifyFile "pulse-modulation_pt2.ipynb" "static\pulse-modulation_pt2-static.ipynb"
+CALL :createAndModifyFile "pulse-modulation_pt3.ipynb" "static\pulse-modulation_pt3-static.ipynb"
+
+REM Exit the script
+exit /b
+
+:createAndModifyFile
+
+REM Copy the input file to the output file in the "static" folder
+copy "%~1" "%~2"
+
+REM Use PowerShell to modify the copied file's content
+powershell -Command "(Get-Content -Raw -Path '%~2') -replace 'static_rendering = False', 'static_rendering = True' | Set-Content -Path '%~2%'"
+
+echo The file "%~2" has been created with the changes applied.
+
+GOTO :EOF
